440a3b91d71d6f38b701cf8319dab105e9fee17b
ciless <ciless.it@gmail.com>
2018-12-01 07:06:07 +0300

    Build

---
+++ dist/precache-manifest.9d5407fa70c4e174dbcfd4269d77df91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42c8d9ca66ef376ddfc0",
+    "url": "/css/app.0e433876.css"
+  },
+  {
+    "revision": "42c8d9ca66ef376ddfc0",
+    "url": "/js/app.a2703ade.js"
+  },
+  {
+    "revision": "5ed614bab03af18a3ca1",
+    "url": "/css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "5ed614bab03af18a3ca1",
+    "url": "/js/chunk-vendors.96c1c699.js"
+  },
+  {
+    "revision": "7894c7a1bed12966b57f996d38020361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
